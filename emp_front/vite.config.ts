5ae6ba3e2a248d2f602ea12139bb98edae43e288
Matitor <yamatitor@gmail.com>
2023-12-19 15:13:14 +0300

    fix

--- emp_front/vite.config.ts
+++ emp_front/vite.config.ts
@@ -11,6 +11,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  base: "/web_front/",
+  //base: "/web_front/",
+  base: "/",
   plugins: [react()],
 })
